6449a3ef7cd0fc633421217056710125c4de4d78
Larry <lunarharmione@gmail.com>
2024-10-13 18:22:51 -0400

    idk

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,9 @@ namespace QuickCalc {
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+       protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+        }
     }
 
 }
--- ViewModels/MenuBarVM.cs
+++ ViewModels/MenuBarVM.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
+using QuickCalc.Models;
+using QuickCalc.Views;
 
 namespace QuickCalc.ViewModels {
     internal class MenuBarVM : ViewModelBase {
@@ -11,9 +8,20 @@ namespace QuickCalc.ViewModels {
         public ICommand OpenSettings {  get; }
 
         public MenuBarVM() {
-            //CloseApp = new RelayCommand(CloseApplication);
-            //OpenSettings = new RelayCommand(OpenSettingsView);
-            
+            CloseApp = new RelayCommand(CloseApplication);
+            OpenSettings = new RelayCommand(OpenSettingsView);
+        }
+
+        private void CloseApplication(object? parameter) {
+            Environment.Exit(0);
+        }
+
+        private void OpenSettingsView(object? parameter) {
+            var mainWindow = (QuickCalc.MainWindow)App.Current.MainWindow;
+            if (mainWindow != null) {
+                mainWindow.MainContent.Content = new SettingsView(); // Ensure this view is defined
+            }
+            //change calculatorview into settingsview
         }
     }
 }
