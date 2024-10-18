ce99ee730cffc309dfe550598e71c6c112e0450e
Larry <lunarharmione@gmail.com>
2024-10-18 01:20:48 -0400

    qol editor-like controls

--- Views/CalculatorView.xaml.cs
+++ Views/CalculatorView.xaml.cs
@@ -28,6 +28,8 @@ namespace QuickCalc.Views
             DataContext = new CalculatorVM();
         }
 
+        //scroll only increments by each line
+        //this will also update how far down the resulting output string will be
         private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e) {
             ScrollViewer scroll = (ScrollViewer)sender;
             double current = scroll.VerticalOffset;
@@ -39,6 +41,29 @@ namespace QuickCalc.Views
             } else if (e.Delta < 0) {
                 scroll.ScrollToVerticalOffset(current+height);
             }
+
+            //need to still add the updating results page
+        }
+
+        //im used to this cus these are the controls for the browsers search bar
+        //this also makes it feel more like a text editor
+        private void InputBox_PreviewKeyDown(object sender, KeyEventArgs e) {
+            int index = InputBox.CaretIndex;
+            int line = InputBox.GetLineIndexFromCharacterIndex(index);
+            int total_lines = InputBox.LineCount;
+
+            switch (e.Key) {
+                case Key.Up: //if we are at the top line, put cursor to the start
+                    if (line == 0) InputBox.CaretIndex = 0;
+                    break;
+                case Key.Down: //if we are at the bottom line, put cursor to the end
+                    if (total_lines == line+1) InputBox.CaretIndex = InputBox.Text.Length;
+                    break;
+            }
+        }
+
+        private void InputBox_Loaded(object sender, RoutedEventArgs e) {
+            InputBox.Focus();
         }
     }
 }
